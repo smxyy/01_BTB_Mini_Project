c19008c988182d36cb571b35084e4ed462d55918
Sokreaksmey Deap <sokreaksmey1827@gmail.com>
2025-03-08 23:58:37 +0700

    add file

--- src/main/java/org/example/model/dao/ProductDao.java
+++ src/main/java/org/example/model/dao/ProductDao.java
@@ -4,6 +4,8 @@ import org.example.custom.exception.CustomException;
 import org.example.model.entity.Product;
 import org.example.model.entity.ProductList;
 
+import java.util.List;
+
 public interface ProductDao {
     ProductList queryAllProducts(int page) throws CustomException;
     int addNewProduct(Product product) throws CustomException;
@@ -11,8 +13,6 @@ public interface ProductDao {
     int updateProductById(int id) throws CustomException;
     int deleteProductById(int id) throws CustomException;
     void setRow(int number);
-    void saveProductToDatabase() throws CustomException;
-    void backUp() throws CustomException;
-    void restoreVersion() throws CustomException;
+    void saveProductToDatabase(List<Product> product, String type) throws CustomException;
     int searchProductByName(String name) throws CustomException;
 }
